44c40d2cb4a3f1aca5f1623d7474df7930bffe4c
Aditi <aditikumari9663@gmail.com>
2023-07-24 14:31:10 +0530

    Initial commit

---
+++ app/src/main/java/com/aditi/recyclerview2/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.aditi.recyclerview2
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var myRecyclerView : RecyclerView
+    lateinit var newsArrayList: ArrayList<News>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        myRecyclerView = findViewById(R.id.recyclerView)
+        val newsImageArray = arrayOf(
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6)
+
+        val newsHeadingArray = arrayOf(
+            "U.K. Foreign Secretary James Cleverly raises issue of BBC tax searches with EAM Jaishankar",
+            "Cooking gas prices hiked by ₹50 for domestic, ₹350.50 for commercial cylinders",
+            "Joe Biden appoints two prominent Indian-American corporate leaders to his Export Council",
+            "Sergey Lavrov will raise suspected bombing of Nord Stream II at G20: Russian Foreign Ministry",
+            "Belarusian leader Lukashenko visits China amid Ukraine tensions",
+            "China rips new U.S. House committee on countering Beijing",
+            "Largest gathering of Foreign Ministers hosted by any G20 presidency: Foreign Secretary Vinay Kwatra"
+        )
+
+        val newsContent = arrayOf(
+            getString(R.string.news_content), getString(R.string.news_content),
+            getString(R.string.news_content), getString(R.string.news_content),
+            getString(R.string.news_content), getString(R.string.news_content)
+        )
+
+// to set items look of items inside recycler view, vertically scrolling, horizontally , uniform grid
+        myRecyclerView.layoutManager = LinearLayoutManager(this)
+        newsArrayList = arrayListOf<News>()
+
+        for (index in newsImageArray.indices){
+            val news = News(newsHeadingArray[index], newsImageArray[index], newsContent[index])
+            newsArrayList.add(news)
+        }
+
+        var myAdapter = MyAdapter(newsArrayList, this)
+        myRecyclerView.adapter = myAdapter
+        myAdapter.setOnItemClickListener(object : MyAdapter.onItemClickListener {
+            override fun onItemClicking(position: Int) {
+                // on clicking action we want to perform
+                val intent = Intent(this@MainActivity,NewsDetailActivity::class.java)
+                intent.putExtra("heading", newsArrayList[position].newsHeading)
+                intent.putExtra("imageId", newsArrayList[position].newsImage)
+                intent.putExtra("newscontent", newsArrayList[position].newsContent)
+                startActivity(intent)
+            }
+        })
+
+    }
+}
---
+++ app/src/main/java/com/aditi/recyclerview2/News.kt
@@ -0,0 +1,3 @@
+package com.aditi.recyclerview2
+
+data class News(var newsHeading: String, var newsImage : Int,var newsContent : String)
